5aef276ffd9fc5b0facc58cbea75fbbdb3f6d4e4
Andrew Lee & Traci <pair+adamandrew.trefong@devbootcamp.com>
2016-06-21 16:34:31 -0700

    Append all teachers after page is loaded using ajax. Move event listener inside the append teachers ajax call so event listeners get attached correctly

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,31 @@
 $.ready(function(){
-  $(".teacher").on("click", function(event){
-    event.preventDefault();
-    var url = this.getAttribute("href")
-    var anchor = this
+  $.ajax({
+    url: "http://spa-badge-api.herokuapp.com/teachers",
+    type: "get"
+  }).then(function(response) {
+    var teachers = JSON.parse(response)
+    var templateScript = SweetSelector.select("#teacher-template")[0].innerText
+    var template = Handlebars.compile(templateScript);
+    var context = {'teachers': teachers}
+    var compiledTemplate = template(context)
+    SweetSelector.select("#teacher-container")[0].innerHTML += compiledTemplate
 
-    $.ajax({
-      url: url,
-      type: "get"
-    }).then(function(response){
-      var object = JSON.parse(response)
-      var badges = object.badges
-      var templateScript = SweetSelector.select("#badge-template")[0].innerText
-      var template = Handlebars.compile(templateScript);
-      var context = {'badges': badges}
-      var compiledTemplate = template(context)
-      SweetSelector.select("#container")[0].innerHTML = compiledTemplate
+    $(".teacher").on("click", function(event){
+      event.preventDefault();
+      var url = this.getAttribute("href")
+
+      $.ajax({
+        url: url,
+        type: "get"
+      }).then(function(response){
+        var teacher = JSON.parse(response)
+        var badges = teacher.badges
+        var templateScript = SweetSelector.select("#badge-template")[0].innerText
+        var template = Handlebars.compile(templateScript);
+        var context = {'badges': badges}
+        var compiledTemplate = template(context)
+        SweetSelector.select("#badge-container" + teacher.id)[0].innerHTML = compiledTemplate
+      })
     })
   })
 })
